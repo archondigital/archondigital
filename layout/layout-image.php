5d30e5816e4473268c0278ea33249e4999a62deb
archondigital <archondigital@users.noreply.github.com>
2016-06-13 05:32:19 +0800

    added switch for tall and wide image orientation for image post format

--- layout/layout-image.php
+++ layout/layout-image.php
@@ -7,54 +7,15 @@
  * @package archondigital
  */
 
+if( get_field('image_orientation_tall') )
+	{
+	    get_template_part( 'layout/layout', 'image-tall' );
+	}
+	else
+	{
+		get_template_part( 'layout/layout', 'image-wide' );
+	}
+
 ?>
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<div class="row fullwidth">
-			<div class="large-4 columns text-right" data-magellan>
-				<?php the_post_thumbnail( 'large' );  // Other resolutions ?>
-			</div>	
-
-			<div class="large-5 columns">
-				<header class="entry-header">
-					<?php
-						if ( is_single() ) {
-							the_title( '<h1 class="entry-title">', '</h1>' );
-						} else {
-							the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-						}
-
-					if ( 'post' === get_post_type() ) : ?>
-					<div class="entry-meta">
-						<?php archondigital_posted_on(); ?>
-					</div><!-- .entry-meta -->
-					<?php
-					endif; ?>
-				</header><!-- .entry-header -->
-
-				<div class="entry-content">
-					<?php
-						the_content( sprintf(
-							/* translators: %s: Name of current post. */
-							wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'archondigital' ), array( 'span' => array( 'class' => array() ) ) ),
-							the_title( '<span class="screen-reader-text">"', '"</span>', false )
-						) );
-
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'archondigital' ),
-							'after'  => '</div>',
-						) );
-					?>
-				</div><!-- .entry-content -->
-
-				<footer class="entry-footer">
-					<?php archondigital_entry_footer(); ?>
-				</footer><!-- .entry-footer -->
-			</div>
-
-			<div class="large-2 columns">	
-			ads go here
-			</div>
-		</div>
-
-	</article><!-- #post-## -->
+	
