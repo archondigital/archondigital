2004d330cf39fab80a29238cabb977f4d10f6e35
archondigital <archondigital@users.noreply.github.com>
2016-06-13 02:56:20 +0800

    added adsense as shortcode

--- functions.php
+++ functions.php
@@ -183,3 +183,18 @@ if( has_post_format('status')) {
 }
   return $data;
 }
+
+// adsense shortcode
+function get_adsense($atts) {
+	return '<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- Archon Digital 2015 -->
+<ins class="adsbygoogle"
+     style="display:inline-block;width:300px;height:250px"
+     data-ad-client="ca-pub-4990847170766317"
+     data-ad-slot="8659334290"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>';
+}
+ 
+add_shortcode('adsense', 'get_adsense');
