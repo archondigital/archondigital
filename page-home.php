453b944a78ae2edb74e4efe8650ead7e39e41ec1
archondigital <archondigital@users.noreply.github.com>
2016-06-13 03:49:48 +0800

    added banners on home page

---
+++ page-home.php
@@ -0,0 +1,17 @@
+<?php
+/*
+Template Name: Home
+*/
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php get_template_part( 'inc/banner', 'main' ); ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
