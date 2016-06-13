d9def3a459b11e7a2e25fa9ffe79e23332e3df66
archondigital <archondigital@users.noreply.github.com>
2016-06-13 13:44:19 +0800

    comment typo on line 50

--- inc/banner-main.php
+++ inc/banner-main.php
@@ -47,7 +47,7 @@
 
     <li class="orbit-slide">
         <div>
-          <img src="<?php// the_field('banner-image'); ?>" alt="Banner image">
+          <img src="<?php the_field('banner-image'); ?>" alt="Banner image">
           <div class="row">
             <div class="large-6 columns"><h3 class="text-right"><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
             <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
