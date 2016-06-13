78357d4421dff25a86d212260aa600181c86b77d
archondigital <archondigital@users.noreply.github.com>
2016-06-13 13:44:59 +0800

    edited text

--- inc/banner-main.php
+++ inc/banner-main.php
@@ -49,7 +49,7 @@
         <div>
           <img src="<?php the_field('banner-image'); ?>" alt="Banner image">
           <div class="row">
-            <div class="large-6 columns"><h3 class="text-right"><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
+            <div class="large-6 columns"><h3><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
             <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
           </div>
         </div>
