c444b83bc26d713e8dd0398005edfd9cf64bedb8
archondigital <archondigital@users.noreply.github.com>
2016-06-13 13:36:21 +0800

    edited text

--- inc/banner-main.php
+++ inc/banner-main.php
@@ -14,7 +14,7 @@
         <div style="height:900px;display:block;">
           <div style="background: url(<?php the_field('banner-image'); ?>); height:800px; width: 100%; background-size:cover; background-position: <?php the_field('banner-position'); ?>; display:block;"></div>
           <div class="row">
-            <div class="large-6 columns"><h3 class="text-right"><small>Featured content: </small><br><?php the_title(); ?></h3></div>
+            <div class="large-6 columns"><h3 class="text-right"><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
             <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
           </div>
         </div>
@@ -49,7 +49,7 @@
         <div style="height:900px;display:block;">
           <div style="background: url(<?php the_field('banner-image'); ?>); height:800px; width: 100%; background-size:cover; background-position: <?php the_field('banner-position'); ?>; display:block;"></div>
           <div class="row">
-            <div class="large-6 columns"><h3 class="text-right"><small>Featured content: </small><br><?php the_title(); ?></h3></div>
+            <div class="large-6 columns"><h3 class="text-right"><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
             <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
           </div>
         </div>
