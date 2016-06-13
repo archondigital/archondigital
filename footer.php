58d4f00136adbdba0159e89e7c06637214fb69d6
archondigital <archondigital@users.noreply.github.com>
2016-06-14 02:02:59 +0800

    added parallax and smooth scroll

--- footer.php
+++ footer.php
@@ -65,5 +65,27 @@
 
 <?php wp_footer(); ?>
 
+<script>
+// stellar
+$(document).stellar();
+//$.stellar();
+
+// scrollTop
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 500);
+        return false;
+      }
+    }
+  });
+});
+</script>
+
 </body>
 </html>
