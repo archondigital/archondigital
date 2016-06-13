114797e131a425906ede7363b9b2a995d384de69
archondigital <archondigital@users.noreply.github.com>
2016-06-13 13:43:23 +0800

    edited banner for mobile

--- inc/banner-main.php
+++ inc/banner-main.php
@@ -46,8 +46,8 @@
   while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
     <li class="orbit-slide">
-        <div style="height:900px;display:block;">
-          <div style="background: url(<?php the_field('banner-image'); ?>); height:800px; width: 100%; background-size:cover; background-position: <?php the_field('banner-position'); ?>; display:block;"></div>
+        <div>
+          <img src="<?php// the_field('banner-image'); ?>" alt="Banner image">
           <div class="row">
             <div class="large-6 columns"><h3 class="text-right"><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
             <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
