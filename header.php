dc845194645a28127486c61294daadc68216823c
archondigital <archondigital@users.noreply.github.com>
2016-06-14 02:49:54 +0800

    added analytics code

--- header.php
+++ header.php
@@ -22,6 +22,18 @@
 
 <body <?php body_class(); ?>>
 
+<!-- Google Analytics -->
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-431457-5', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text hide" href="#main"><?php esc_html_e( 'Skip to content', 'archondigital' ); ?></a>
 		
