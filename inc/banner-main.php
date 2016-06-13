38c602abf8aafba5c08ebf4a06329f1ca623fb84
archondigital <archondigital@users.noreply.github.com>
2016-06-13 13:47:13 +0800

    edited padding

--- inc/banner-main.php
+++ inc/banner-main.php
@@ -15,7 +15,7 @@
           <div style="background: url(<?php the_field('banner-image'); ?>); height:800px; width: 100%; background-size:cover; background-position: <?php the_field('banner-position'); ?>; display:block;"></div>
           <div class="row">
             <div class="large-6 columns"><h3 class="text-right"><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
-            <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
+            <div class="large-6 columns"><figcaption><?php the_field('banner-caption'); ?></figcaption></div>
           </div>
         </div>
     </li>
@@ -50,7 +50,7 @@
           <img src="<?php the_field('banner-image'); ?>" alt="Banner image">
           <div class="row">
             <div class="large-6 columns"><h3><small>Featured content:</small><br> <?php the_title(); ?></h3></div>
-            <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
+            <div class="large-6 columns"><figcaption><?php the_field('banner-caption'); ?></figcaption></div>
           </div>
         </div>
     </li>
