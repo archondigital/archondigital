ebc90e3635cd92fb9d372abe53578e3cec86faaf
archondigital <archondigital@users.noreply.github.com>
2016-06-13 13:34:47 +0800

    edited banner style

--- inc/banner-main.php
+++ inc/banner-main.php
@@ -46,9 +46,12 @@
   while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
     <li class="orbit-slide">
-        <div>
-          <img src="<?php the_field('banner-image'); ?>" alt="Banner image">
-          <figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption>
+        <div style="height:900px;display:block;">
+          <div style="background: url(<?php the_field('banner-image'); ?>); height:800px; width: 100%; background-size:cover; background-position: <?php the_field('banner-position'); ?>; display:block;"></div>
+          <div class="row">
+            <div class="large-6 columns"><h3 class="text-right"><small>Featured content: </small><br><?php the_title(); ?></h3></div>
+            <div class="large-6 columns"><figcaption style="padding:1.25rem;"><?php the_field('banner-caption'); ?></figcaption></div>
+          </div>
         </div>
     </li>
 
