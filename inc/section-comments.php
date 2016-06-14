9ecad65242e8cb7abf3d513aed4030683a888677
archondigital <archondigital@users.noreply.github.com>
2016-06-14 13:11:02 +0800

    added a switch to enable or disable disqus from appearing in posts

--- inc/section-comments.php
+++ inc/section-comments.php
@@ -36,30 +36,38 @@ if ( post_password_required() ) {
 
 		<hr>
 
-		<div id="disqus_thread"></div>
-		<script>
-		    /**
-		     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-		     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-		     */
-		    
-		    var disqus_config = function () {
-		        
-		        this.page.identifier = '<?php the_field ('dsq_thread_id') ?>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-		        this.page.url = '<?php the_permalink(); ?>';  // Replace PAGE_URL with your page's canonical URL variable
-		        
-		    };
-		    
-		    (function() {  // DON'T EDIT BELOW THIS LINE
-		        var d = document, s = d.createElement('script');
-		        
-		        s.src = '//archondigital.disqus.com/embed.js';
-		        
-		        s.setAttribute('data-timestamp', +new Date());
-		        (d.head || d.body).appendChild(s);
-		    })();
-		</script>
-		<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>	
+		<?php global $wp_query; $postid = $wp_query->post->ID; wp_reset_query(); ?>
+		<?php $key = 'enable_disqus'; $themeta = get_post_meta($post->ID, $key, TRUE); if($themeta != '') { ?>
+		    <div id="disqus_thread"></div>
+			<script>
+			    /**
+			     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+			     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
+			     */
+			    
+			    var disqus_config = function () {
+			        
+			        this.page.identifier = '<?php the_field ('dsq_thread_id') ?>'; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+			        this.page.url = '<?php the_permalink(); ?>';  // Replace PAGE_URL with your page's canonical URL variable
+			        
+			    };
+			    
+			    (function() {  // DON'T EDIT BELOW THIS LINE
+			        var d = document, s = d.createElement('script');
+			        
+			        s.src = '//archondigital.disqus.com/embed.js';
+			        
+			        s.setAttribute('data-timestamp', +new Date());
+			        (d.head || d.body).appendChild(s);
+			    })();
+			</script>
+			<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>	
+
+		<?php } else { ?>
+			
+		<?php } ?>		
+
+		
 
 		<?php // If comments are open or we have at least one comment, load up the comment template.
 			//if ( comments_open() || get_comments_number() ) :
