091613d8b4f6decf6eba99b6d98160f6cbc8adef
archondigital <archondigital@users.noreply.github.com>
2016-07-18 01:28:08 +0800

    bg image fix

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 	<?php if(has_post_thumbnail()) :?>
-	<div style="background-image: url(<?php the_post_thumbnail_url(); ?>); padding: 40rem 0 0 0; background-size:cover; background-position:top center; margin-bottom:2.25rem; background-repeat:no-repeat;" data-stellar-background-ratio="0.3">
+	<div style="background-image: url(<?php the_post_thumbnail_url(); ?>); padding: 40rem 0 0 0; background-size:103%; background-position:top center; margin-bottom:2.25rem; background-repeat:no-repeat;" data-stellar-background-ratio="0.3">
 		<div class="row fullwidth" style="background: rgba(0,0,0,0.5);">
 			<div class="large-12 columns">
 				<header class="entry-header" style="padding:2.25rem 0 0 0;">
