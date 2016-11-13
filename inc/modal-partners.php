b0ae653aa4e7c04ad99a0d2e945de9672fe61c4c
archondigital <archondigital@users.noreply.github.com>
2016-11-13 19:01:29 +0800

    partner modal edits

--- inc/modal-partners.php
+++ inc/modal-partners.php
@@ -1,8 +1,8 @@
 <div id="modal-partners" class="reveal" data-reveal>
   <h2 id="modalTitle">Partners and Blogroll.</h2>
   <p class="lead">This is where I place tracking widgets of blog networks such as those listed below.</p>
-  <p><a href="http://www.topblogs.com.ph/web-development/"><img style="border:none;" src="http://www.topblogs.com.ph/track_7510.gif" alt="Web Development & Design - Top Blogs Philippines" /></a></p>
-  
+  <p><a href="http://www.topblogs.com.ph/web-development/"><img style="border:none" src="http://www.topblogs.com.ph/track_7510.gif" alt="Web Development &amp; Design - Top Blogs Philippines" /></a></p>
+
   <button class="close-button" data-close aria-label="Close modal" type="button">
     <span aria-hidden="true">&times;</span>
   </button>
