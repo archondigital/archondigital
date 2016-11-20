3691d5c49de50cdb754f8ce6a8616254920b9199
archondigital <archondigital@users.noreply.github.com>
2016-11-20 10:04:33 +0800

    edited responsive adsense

--- inc/ads-adsense03.php
+++ inc/ads-adsense03.php
@@ -2,7 +2,7 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Archon Digital - Photo Post Ad -->
 <ins class="adsbygoogle"
-     style="display:block"
+     style="display:inline-block;min-width:320px;max-width:970px;width:100%;"
      data-ad-client="ca-pub-4990847170766317"
      data-ad-slot="6237674294"
      data-ad-format="auto"></ins>
@@ -10,3 +10,5 @@
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 <!-- end adsense -->
+
+style="display:inline-block;min-width:400px;max-width:970px;width:100%;
