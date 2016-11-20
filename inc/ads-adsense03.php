ef33a70c08b3007996c2a73124bb12e0f703dd13
archondigital <archondigital@users.noreply.github.com>
2016-11-20 10:09:28 +0800

    reverted photo ad

--- inc/ads-adsense03.php
+++ inc/ads-adsense03.php
@@ -2,7 +2,7 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <!-- Archon Digital - Photo Post Ad -->
 <ins class="adsbygoogle"
-     style="display:inline-block;min-width:320px;max-width:970px;width:100%;"
+     style="display:block"
      data-ad-client="ca-pub-4990847170766317"
      data-ad-slot="6237674294"
      data-ad-format="auto"></ins>
@@ -10,5 +10,3 @@
 (adsbygoogle = window.adsbygoogle || []).push({});
 </script>
 <!-- end adsense -->
-
-style="display:inline-block;min-width:400px;max-width:970px;width:100%;
